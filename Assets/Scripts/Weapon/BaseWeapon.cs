e28154e379ff9471d873ffed0c17629fe30ab152
dhpham <dayvidpham@gmail.com>
2023-06-11 12:14:58 -0700

    Added skeleton for different weapons/swapping

--- Assets/Scripts/Weapon/BaseWeapon.cs
+++ Assets/Scripts/Weapon/BaseWeapon.cs
@@ -6,53 +6,69 @@ using CommonDefinitions;
 
 public class BaseWeapon : MonoBehaviour, IWeapon
 {
-    // May not be able to attack: out of ammo, various reasons
-    [field: SerializeField] public bool CanAttack { get; set; }
-
-    // Determines how fast the attack is
     [SerializeField]
-    protected float _attackSpeed;
-
-    // Associated weapon sound
-    [SerializeField]
-    protected AudioClip _attackSound;
+    private AudioClip _attackSound;
 
     // Weapon collider hitbox
     [SerializeField]
-    protected Collider _collider;
+    private Collider _collider;
+
+    // Determines how fast the attack is
+    [SerializeField]
+    private float _attackSpeed = 1.0f;
+
+    // May not be able to attack: out of ammo, various reasons
+    [field: SerializeField] public bool CanAttack { get; set; }
 
     // What operator is active: what will occur on hit
-    [field: SerializeField] public Operators ActiveOperator { get; set; }
+    [field: SerializeField] public Operators ActiveOperator { get; set; } = Operators.Subtraction;
 
 
     // Start is called before the first frame update
     public void Start()
     {
-        this._attackSound = GetComponent<AudioClip>();
-        this._collider = GetComponent<Collider>();
+        _attackSound = GetComponent<AudioClip>();
+        _collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     public void Update()
     {
-
+        // do nothing
     }
 
     /**
-     * Exists for side-effects (spawning projectiles): doesn't do anything right now for melee attacks
+     * Exists for side-effects (e.g. spawning projectiles): doesn't do anything right now for melee attacks
      */
     public void Attack()
     {
-
+        // do nothing
     }
 
-    // On hit
+    /**
+     * What occurs on hit
+     */
     public void OnTriggerEnter(Collider other)
     {
+        BaseEnemy enemy = other.GetComponent<BaseEnemy>();
+        // BasePlayer player = ??? how to get player ref
+        if (!enemy) return; // no enemy collision: do nothing
 
+
+        switch(ActiveOperator)
+        {
+            case Operators.Addition:
+                break;
+            case Operators.Subtraction:
+                break;
+            case Operators.Multiplication:
+                break;
+            case Operators.Division:
+                break;
+        }
     }
 
-    public virtual bool TryAttack()
+    public bool TryAttack()
     {
         if(!CanAttack)
         {
@@ -62,7 +78,7 @@ public class BaseWeapon : MonoBehaviour, IWeapon
         return true;
     }
 
-    public virtual void SetColliderEnabled(bool colliderEnabled)
+    public void SetColliderEnabled(bool colliderEnabled)
     {
         _collider.enabled = colliderEnabled;
     }
