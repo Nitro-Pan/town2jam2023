2f3ab96aa77d6fb9938c15fcf44d9145e0840422
Joshua Krauchi <jkrauchi@my.bcit.ca>
2023-06-10 18:23:21 -0700

    Enemy work.

---
+++ Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AI/Nodes/BaseAIBehaviourNode.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BaseAIBehaviorNode : IAINode
+{
+    public bool CanEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public AINodeResult TryRun()
+    {
+        throw new System.NotImplementedException();
+    }
+}
