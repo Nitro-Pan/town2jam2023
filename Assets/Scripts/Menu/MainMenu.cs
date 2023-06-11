6bc689b074a845194be10d77751be7d6fc2518b1
Mazianos <mazin.marwan@gmail.com>
2023-06-11 16:22:34 -0700

    Menus

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -9,27 +9,11 @@ public class MainMenu : MonoBehaviour
 {
     public void Start()
     {
-        var playerInput = gameObject.AddComponent<PlayerInput>();
 
-        InputActionAsset inputActionAsset = ScriptableObject.CreateInstance("InputActionAsset") as InputActionAsset;
-        playerInput.actions = inputActionAsset;
-
-        var inputActionMap = new InputActionMap("ChangerMap");
-        inputActionAsset.AddActionMap(inputActionMap);
-
-        var inputAction = inputActionMap.AddAction("ChangerAction", type: InputActionType.Button, binding: "<Mouse>/leftButton");
-        inputAction.Enable();
-
-        playerInput.currentActionMap = inputActionMap;
-
-        inputAction.started += PlayGame;
-
-        inputActionAsset.Enable();
     }
-    public void PlayGame(InputAction.CallbackContext context)
+    public void PlayGame()
     {
         Time.timeScale = 1f;
-        Debug.Log("Playing!");
         SceneManager.LoadScene("Level_V0");
     }
     public void QuitGame()
