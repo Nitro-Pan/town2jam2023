665605b6c7ce1a2c5c72a03a6b3101693f942ec9
Mazianos <mazin.marwan@gmail.com>
2023-06-11 12:27:59 -0700

    Menus temp

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level_V0");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
